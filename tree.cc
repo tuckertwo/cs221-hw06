d10f989f5cdc58dd357c778d1a62693e4a305b1f
Tucker R. Twomey <tucker@the-twomeys.com>
2022-03-11 12:30:57 -0800

    Fix field names and add comment

--- tree.cc
+++ tree.cc
@@ -12,7 +12,10 @@ tree_ptr_t create_tree(const key_type& key,
 //////////////////////////////////////////////////////////////////////////////
 void destroy_tree(tree_ptr_t tree)
 {
-  for(tree_ptr_t branch : {tree->left, tree->right})
+  // This for loop can be confusing; the tree traversal is done recursively.
+  // (The for loop is just here to iterate over the left and right nodes to
+  // eliminate some cut-and-pasting of code.)
+  for(tree_ptr_t branch : {tree->left_, tree->right_})
   {
     if(branch)
     {
